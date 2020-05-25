8c3f0dfee3deae82d189c1b55b2dd8d588b4639f
whg <wanghg@skylinematrix.com>
2020-05-26 03:04:59 +0800

    代码整理

--- js/game.js
+++ js/game.js
@@ -1,75 +1,48 @@
-﻿
-var div1, div2, div3, loading, mode = 1, ready = false;
+﻿var div1, div2, div3, loading, mode = 1, ready = false;
 
 var Game = function(){
-
 	var init = function( hash ){
-		
 		if ( hash ){
 			mode = hash;
 			return gameStart();
 		}
+
 		loading = document.createElement( 'img' );
-		
 		loading.src = 'images/g/loading.jpg';
+        document.body.appendChild( loading );
 
+		var wrap = document.body.appendChild( document.createElement( "div" ) );
 
-
-		var wrap = document.body.appendChild( document.createElement( "div" ) )
-
-		document.body.appendChild( loading );
-		
 		div = document.body.appendChild( document.createElement( 'div' ) );
-		
 		div.style.position = 'absolute';
-		
 		div.style.top = '200px';
-		
 		div.style.left = '450px';
-
-		div.style.fontSize = '45px'
-
+		div.style.fontSize = '45px';
 		div.style.color = '#fff';
-	
 		div.style.width = '500px';
-		
-		div.style.height = '100px'
-
+		div.style.height = '100px';
 		div.innerHTML = 'loading...';
 
+		//加载图片数目显示
 		div2 = document.body.appendChild( document.createElement( 'div' ) );
-				
 		div2.style.position = 'absolute';
-		
 		div2.style.top = '300px';
-		
 		div2.style.left = '450px';
-
-		div2.style.fontSize = '35px'
-		
+		div2.style.fontSize = '35px';
 		div2.style.color = '#fff';
-		
 		div2.style.width = '500px';
-		
 		div2.style.height = '500px';
-		
-	
 
-
-		document.body.style.position = 'relative'
-		document.body.style.left = ( document.body.offsetWidth  - 900 ) / 2 + 'px'
-		document.body.style.top = '20px'
+		document.body.style.position = 'relative';
+		document.body.style.left = ( document.body.offsetWidth  - 900 ) / 2 + 'px';
+		document.body.style.top = '20px';
 		document.body.style.overflow = 'hidden';
 		document.body.style.backgroundColor = '#A0AAB2';
 
 
-		
-		Util.loadImg( function(){	
-			
+		Util.loadImg( function(){
 			Util.loadAudio( function(){
-			
 				gameStart();
-
 
 				var div = document.createElement( 'div' );
 				div.style.width = '720px';
@@ -80,98 +53,72 @@ var Game = function(){
 				div.style.zIndex = 9999;
 				div.innerHTML = '作者blog: <a href="http://www.alloyteam.com/author/tat-svenzeng/" target="_blank">http://www.alloyteam.com/author/tat-svenzeng/</a>'
 				document.body.appendChild( div );
-
-	
 			})
-		
 		});
 
-
-	}
+	};
 
 	return {
 		init: init
 	}
-
-
 }();
 
 
-
 var gameStart = function(){
-	
 		document.body.removeChild( div );
-	
 		document.body.removeChild( div2 );
-	
 		document.body.removeChild( loading );
 			
 		Timer.start();
 	
 		window.player1 = Spirit.getInstance( Config.Spirit.RYU1 );
-	
 		window.player2 = Spirit.getInstance( Config.Spirit.RYU2 );
-	
 		player1.setEnemy( player2 );
-	
 		player2.setEnemy( player1 );
-
 		player1.bloodBar = Blood.leftBar();
-	
 		player2.bloodBar = Blood.rightBar();
 	
 		window.map = Map.init();
-				
 		window.Stage = Stage();
-
 		Spirit.interface( 'Stage', Stage );
 
 		player1.init( 280, 240, 1 );   //left, top, direction
-	
 		player2.init( 480, 240, -1 );  //left, top, direction
 	
 		Blood.init();
 		
 		player2.keyManage.stop();
-
 		player2.ai = player2.implement( 'Ai' );
-
 		player2.ai.start();
-
 		player2.enemy.bloodBar.event.listen( 'empty', function(){
 			player2.ai.stop();
-		})
+		});
 
 		var pause = false, lock = false;
-	
 		document.onkeydown = function( ev ){
 			var ev = ev || window.event;
-			var keycode = ev.keyCode;
-			if ( keycode === 113 ){
+			var keyCode = ev.keyCode;
+			if ( keyCode === 113 ){
 				( pause = !pause ) ? Timer.stop() : Timer.start();
 			}
-			if ( keycode === 50 || keycode === 49 ){
+			if ( keyCode === 50 || keyCode === 49 ){
 				if ( lock ) return;
 				lock = true;
-				mode = keycode - 48;
+				mode = keyCode - 48;
 				player2.ai.stop();
 				Game.reload();
 				setTimeout( function(){
 					lock = false;
-				}, 1000 )
+				}, 1000 );
 			}
 		}	
-	
-	
-}
+};
 
 
 var Blood = function(){
 	
 	var div;
-	
 	var init = function(){
-
 		div = document.createElement( 'div' );
 		div.style.width = '720px';
 		div.style.height = '32px';
@@ -187,25 +134,19 @@ var Blood = function(){
 		img.style.height = '32px';
 		img.src = 'images/g/bar.gif';
 		div.appendChild( img );
-
-	}
+	};
 	
 	var reload = function(){
-		
 		var d = document.body;
-
 		while( d.firstChild != null && d.firstChild.tagName !== 'CANVAS'  ){
 			d.removeChild( d.firstChild );
 		}
-
 		init();
-	}
+	};
 	
-
 	var animateWidth = function( timeAll, _f_width, width ){
 
 		var f_time = +new Date;
-
 		var easing = Config.easing[ 'linear' ], timeoutfn;
 
 		var move = function(){
@@ -216,30 +157,28 @@ var Blood = function(){
 				timeoutfn && timeoutfn();
 			}
 			return w;
-		}
+		};
 
 		timeout = function( fn ){
 			w = _f_width + width;
 			timeoutfn = fn;
-		}
+		};
 		
 		var fireTimeout = function(){
 			w = _f_width + width;
 			timeoutfn && timeoutfn();
-		}
+		};
 
 		return {
 			move: move,
 			timeout: timeout,
 			fireTimeout: fireTimeout
 		}
-	}
+	};
 
 
 	var leftBar = function(){
-
 		var div = document.createElement( 'div' );
-
 		div.style.top = '40px';
 		div.style.left = '96px';
 		div.style.width = '322px';
@@ -248,24 +187,18 @@ var Blood = function(){
 		div.style.position = 'absolute';
 		div.style.zIndex = 9999;
 		div.style.border = '1px #fff solid';
-
 		document.body.appendChild( div );
 
 		var _blood = 1500, _f_left = 96, _f__blood = 1500, _f_width = 322, currWidth = _f_width, timer, animate, emptyfn;
-		
 		var event = Event();
-		
+
 		var framefn = function(){
-
 			var w = animate.move();
-
 			div.style.width = w + 'px';
 			div.style.left = Math.min( _f_left + _f_width - w , _f_left + _f_width )+ 'px';
-
-		}
+		};
 		
 		var firing = false;
-		
 		timer = Timer.add( framefn );
 
 		var reduce = function( count ){
@@ -296,20 +229,18 @@ var Blood = function(){
 			
 			firing = true;
 
-		}
+		};
 
 		var reload = function(){
 			reduce( _blood - _f__blood );
-		}
-		
+		};
 		
 		return {
 			reduce: reduce,
 			event: event,
 			reload: reload
 		}
-		
-	}
+	};
 
 
 	var rightBar = function(){
@@ -325,19 +256,14 @@ var Blood = function(){
 		document.body.appendChild( div );
 
 		var _blood = 1500, _f_left = 493, _f__blood = 1500, _f_width = 320, currWidth = _f_width, timer, animate, emptyfn, queue = Interfaces.Queue();
-
 		var event = Event();
 		
 		var framefn = function(){
-
 			var w = animate.move();
-
 			div.style.width = w + 'px';
-
-		}
+		};
 		
 		var firing = false;
-		
 		timer = Timer.add( framefn );
 
 		var reduce = function( count ){
@@ -368,53 +294,49 @@ var Blood = function(){
 			
 			firing = true;
 
-		}
+		};
 		
 		var empty	= function( fn ){
 			emptyfn = fn;
-		}
+		};
 
 		var reload = function(){
 			reduce( _blood - _f__blood );
-		}
+		};
 		
 		return {
 			reduce: reduce,
 			event: event,
 			reload: reload
 		}
+	};
 
-	}
-	
-	
 	return {
 		init: init,
 		leftBar: leftBar,
 		rightBar: rightBar,
 		reload: reload
 	}
-	
-}()
-
+}();
 
 
 window.onload = function(){
 	Game.init();
-}
-
+};
 
 Game.reload = function(){
 	player1.keyManage.stop();
 	player2.keyManage.stop();
 	player1.bloodBar.reload();
 	player2.bloodBar.reload();
+
 	setTimeout( function(){
 		player1.play( 'force_wait', 'force' );
 		setTimeout( function(){
 		 player1.animate.moveto( 280, 240 );
 		 player1.keyManage.start();
 		 player1.direction = 1;
-		}, 30 )
+		}, 30 );
 
 		player2.play( 'force_wait', 'force' );
 		setTimeout( function(){
@@ -424,9 +346,7 @@ Game.reload = function(){
 		 if ( mode === 1 ){
 		 	player2.ai.start();
 		 }
-		}, 30 )
+		}, 30 );
 		
-	}, 1000 )
-
-
-}
+	}, 1000 );
+};
--- js/util.js
+++ js/util.js
@@ -1,5 +1,3 @@
-
-
 var Util = {
 
 	isArray: function( ary ){
@@ -25,13 +23,11 @@ var Util = {
 		RYU1: [ 'RYU1_wait.gif', 'RYU1_goForward.gif', 'RYU1_goBack.gif', 'RYU1_jumpUp.gif', 'RYU1_jump_down.gif', 'RYU1_impact_boxing.gif', 'RYU1_after_impact_boxing.gif', 'RYU1_before_whirl_kick.gif', 'RYU1_after_whirl_kick.gif', 'RYU1_whirl_kick.gif', 'RYU1_heavy_kick.gif', 'RYU1_jump_forward.gif', 'RYU1_crouch_heavy_kick.gif', 'RYU1_stand_up.gif', 'RYU1_crouch.gif', 'RYU1_jump_forward.gif', 'RYU1_jump_back.gif', 'RYU1_light_boxing.gif',
 			'RYU1_crouch_light_boxing.gif', 'RYU1_light_kick.gif', 'RYU1_middle_boxing.gif', 'RYU1_crouch_middle_boxing.gif', 'RYU1_crouch_light_kick.gif', 'RYU1_wave_boxing.gif', 'RYU1_jump_light_kick.gif', 'RYU1_jumpMoved_middle_kick.gif', 'RYU1_jump_heavy_kick.gif', 'RYU1_jump_light_boxing.gif', 'RYU1_jump_middle_boxing.gif', 'RYU1_jumpMoved_light_kick.gif', 'RYU1_jump_middle_boxing.gif', 'RYU1_jumpMoved_light_kick.gif', 'RYU1_near_light_boxing.gif', 'RYU1_near_heavy_boxing.gif', 'RYU1_near_light_kick.gif', 'RYU1_near_middle_kick.gif', 'RYU1_near_heavy_kick.gif', 'RYU1_stand_up_defense.gif', 'RYU1_stand_crouch_defense.gif',
 			'RYU1_beAttacked_fall.gif', 'RYU1_somesault_up.gif', 'RYU1_beAttacked_top.gif', 'RYU1_beAttacked_bottom.gif', 'RYU1_beAttacked_heavy.gif', 'RYU1_before_fall_down.gif', 'RYU1_fall_down.gif', 'RYU1_beAttacked_impact.gif'
-		
 		],
 
 		RYU2: [ 'RYU2_wait.gif', 'RYU2_goForward.gif', 'RYU2_goBack.gif', 'RYU2_jumpUp.gif', 'RYU2_jump_down.gif', 'RYU2_impact_boxing.gif', 'RYU2_after_impact_boxing.gif', 'RYU2_before_whirl_kick.gif', 'RYU2_after_whirl_kick.gif', 'RYU2_whirl_kick.gif', 'RYU2_heavy_kick.gif', 'RYU2_jump_forward.gif', 'RYU2_crouch_heavy_kick.gif', 'RYU2_stand_up.gif', 'RYU2_crouch.gif', 'RYU2_jump_forward.gif', 'RYU2_jump_back.gif', 'RYU2_light_boxing.gif',
 			'RYU2_crouch_light_boxing.gif', 'RYU2_light_kick.gif', 'RYU2_middle_boxing.gif', 'RYU2_crouch_middle_boxing.gif', 'RYU2_crouch_light_kick.gif', 'RYU2_wave_boxing.gif', 'RYU2_jump_light_kick.gif', 'RYU2_jumpMoved_middle_kick.gif', 'RYU2_jump_heavy_kick.gif', 'RYU2_jump_light_boxing.gif', 'RYU2_jump_middle_boxing.gif', 'RYU2_jumpMoved_light_kick.gif', 'RYU2_jump_middle_boxing.gif', 'RYU2_jumpMoved_light_kick.gif', 'RYU2_near_light_boxing.gif', 'RYU2_near_heavy_boxing.gif', 'RYU2_near_light_kick.gif', 'RYU2_near_middle_kick.gif', 'RYU2_near_heavy_kick.gif', 'RYU2_stand_up_defense.gif', 'RYU2_stand_crouch_defense.gif',
 			'RYU2_beAttacked_fall.gif', 'RYU2_somesault_up.gif', 'RYU2_beAttacked_top.gif', 'RYU2_beAttacked_bottom.gif', 'RYU2_beAttacked_heavy.gif', 'RYU2_before_fall_down.gif', 'RYU2_fall_down.gif', 'RYU2_beAttacked_impact.gif'
-		
 		],
 
 		hitEffect: [ 'defense.gif', 'heavy.gif', 'light.gif' ],
@@ -49,7 +45,6 @@ var Util = {
 	},
 	
 	loadImg: function( callback ){
-
 		var root = 'images/', count = 0, num = 0;
 
 		for ( var i in this.images ){
@@ -68,14 +63,14 @@ var Util = {
 						obj: this,
 						width: this.width,
 						height: this.height
-					}
+					};
 					num += 1;
 					div2.innerHTML = num + '/' + count;
 					if ( count === num ){
 						callback && callback();
 					}
 					this.onload = null;
-				}
+				};
 				image.src = src;
 			}
 		}
@@ -99,10 +94,4 @@ var Util = {
 		}
 	}
 
-	
-	
-}
-
-
-
-
+};
